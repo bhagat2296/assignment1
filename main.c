0a13e94775953020fa163474051f678090887094
bhagat2296 <103653090+bhagat2296@users.noreply.github.com>
2022-07-26 00:21:04 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int r;
+    int dd,mm,yy,hh,mn;
+    float area;
+    //string bhagat;
+    printf("Hello Students\n");// Question 1
+
+    printf("Hello \nStudents\n");// Question 2
+
+    printf("\"My SirG\"\n"); // Question 3
+
+   /* printf("enter radius\n");// Question 4
+    scanf("%d",&r);
+    area=3.14*r*r;
+
+    printf("Radius of circle is %f",area);*/
+
+    int x=printf("bhagatsingh");// Question 5
+
+    printf("\nLENGTH OF ABOVE STRING IS %d\n",x);
+
+    printf("\"Hello \,Bhagat Singh\"\n");// Question 6
+
+    //printf("\"%d\"\n");// Question 7
+
+    printf("\"\\n\"\n");// Question 8
+
+    printf("\"\\\\\"\n");// Question 9
+
+    /*printf(" ENTER DATE \"DD\\MM\\YYYY\"\n"); //Question 10
+    scanf("%d/%d/%d",&dd,&mm,&yy);
+
+    printf("Day-dd, Month-mm, year-yy(Day-%d, Month-%d, Year-%d)",dd,mm,yy);*/
+
+   /* printf("Enter Time in Hour and Minutes format\n");// Question 11
+    scanf("%d:%d",&hh,&mn);
+
+    printf("%d Hour and %d Minutes",hh,mm);*/
+
+    //7 for question 12
+
+
+    return 0;
+}
